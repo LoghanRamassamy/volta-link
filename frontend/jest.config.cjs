fb4a10d93bdeba6dda9746cab5b8fefa92345505
Loghan Ramassamy <loghan.ramassamy@tyliainvest.com>
2026-06-05 20:36:38 +0200

    chore: remove legacy jest config files since vitest is used

--- frontend/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.test.ts', '**/*.test.tsx'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { ...require('./tsconfig.json').compilerOptions, module: 'commonjs' } }],
-  },
-};
